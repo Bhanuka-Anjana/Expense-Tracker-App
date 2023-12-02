6c4c962d8fbc618cd5c60e5d1537ff4211c900dd
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-12-02 09:24:03 +0530

    init front end

---
+++ expense-client/src/services/expenses.js
@@ -0,0 +1,15 @@
+
+export const GetExpenses = async (dispatch)=>{
+    try {
+        //api call
+        const expenses = [
+            {id:1, description:'Groceries',amount:23.16},
+            {id:2, description:'Gas',amount:23.16},
+            {id:3, description:'Loan',amount:23.16},
+            {id:4, description:'Travel',amount:23.16},
+        ]
+    } catch {
+        console.log("Error!");
+    }
+        
+} 
